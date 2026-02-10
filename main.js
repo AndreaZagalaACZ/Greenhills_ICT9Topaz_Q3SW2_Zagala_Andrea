d84e8260471c30963b584d2b889e56fb44bae01d
Andrea Zagala <zagala.ac@obmontessori.edu.ph>
2026-02-10 13:50:41 +0800

    Display estimated discount in output element
    Update the alert to display the discount in the output element.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function compute_discount() {
      value = document.getElementById('value').value;
     let discount = value * 0.20;
-    window.alert('Your estimated discount is ' + discount);
+     document.getElementById('output').textContent = 'Your estimated discount is ' + discount;
 }
